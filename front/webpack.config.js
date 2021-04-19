5e4c45f7cb21223f05553cc9b1a415816e6b83a8
JF Brillant <jf.brillant@live.fr>
2021-04-20 00:07:10 +0200

    Setting working directory and install npm packages

---
+++ front/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: {
+    polyfill: "babel-polyfill",
+    app: "./src/index.js"
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    inline:true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
